d4aded55d434fb0040d4d82d2e8aa481763737ee
Erick Tomaz <ericktomazs@hotmail.com>
2024-05-05 23:31:01 +0100

    connect notifications app with payments app

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '@app/common';
+import { NOTIFICATION_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from '../dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -11,15 +13,24 @@ export class PaymentsService {
       apiVersion: '2024-04-10',
     },
   );
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATION_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ amount, email }: PaymentsCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
       currency: 'usd',
       payment_method: 'pm_card_visa',
       payment_method_types: ['card'],
+    });
+
+    this.notificationsService.emit('notify_email', {
+      email,
+      text: 'your payment was successfully done',
     });
 
     return paymentIntent;
